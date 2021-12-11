5381fc643bd558ca6414c1b09e7cfeeaed4b37ec
Subas <75614431+subashbashyal@users.noreply.github.com>
2021-12-11 11:10:19 +0545

    subas commit

---
+++ app/map.js
@@ -0,0 +1,61 @@
+// const items = [
+//     { name:'Bike',   price:100},
+//     { name: 'Tv',    price:200},
+//     { name: 'Album', price:10},
+//     { name: 'book',   price:5},
+//     { name: 'Computer', price:999},
+//     {name: 'keyboard',   price:25}
+  
+// ]
+//Filter item in an array
+// const filteredItems = items.filter((item) => {
+//     return item.price <=100
+// }) 
+// console.log(items);
+// console.log(filteredItems);
+
+//mapfunctions
+// const itemNames = items.map((item) => {
+//     return item.price
+// })
+// console.log(itemNames);
+
+//findFunction
+// const foundItem = items.find((item) => {
+//     return item.name === 'Album'//Main items to be find
+// })
+// console.log(foundItem);
+
+//forEach Function
+// items.forEach((item)=>{
+//    console.log(item.name) 
+// })
+
+// For Some items in an array
+// only returns true or false  
+// const hasInexpensiveItems = items.some((item) => {
+//     return item.price <= 100 
+// })
+
+// console.log(hasInexpensiveItems)
+
+//everyFunction
+// const hasInexpensiveItems = items.every((item)=>{
+//     return item.price <= 999
+// })
+
+// console.log(hasInexpensiveItems);
+
+//ReduceFunction:oes through each items
+// const total = items.reduce((currentTotal, item) => {
+//     return item.price + currentTotal
+// },0)
+// console.log(total)
+
+// includesfunction.{to check values in function}
+// const items =[1, 2, 3, 4, 5]
+// const includesTwo = items.includes(7)
+
+// console.log(includesTwo)
+
+
